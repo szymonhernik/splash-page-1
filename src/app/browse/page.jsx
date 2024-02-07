ca887e71d200f61cef54543de8d4e7d0df29773c
szy her <szymonhernik123@gmail.com>
2024-02-07 14:55:10 +0100

    clean start

--- src/app/browse/page.jsx
+++ src/app/browse/page.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Page() {
   return (
     <div className="bg-neutral-800 flex-1 overflow-y-auto ">
-      <div className="grow h-full w-[calc(100vw-16rem)]">
+      <div className="grow h-full flex justify-center items-center w-[calc(100vw-16rem)] underline">
         <Link href="/">go back to canvas</Link>
       </div>
     </div>
